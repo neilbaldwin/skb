1f92c6759b37ac1bd8bab033a7b16c9e814310e4
Neil Baldwin <neil.baldwin@mac.com>
2021-05-11 08:21:40 +0100

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-#!/usr/bin/env python
-from setuptools import setup
-from importlib.metadata import version
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+with open('requirements.txt') as f: 
+    requirements = f.readlines() 
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+        version ='1.2.1', 
 )
